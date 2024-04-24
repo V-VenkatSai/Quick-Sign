8f7a8a9671429db86e307c74984f35019b4bb669
Venkat Sai <165140024+V-VenkatSai@users.noreply.github.com>
2024-04-24 21:16:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+const colorPicker = document.getElementById("colorPicker");
+const canvasColor = document.getElementById("canvasColor");
+const canvas = document.getElementById("myCanvas");
+const clearButton = document.getElementById("clearButton");
+const saveButton = document.getElementById("saveButton");
+const retrieveButton = document.getElementById("retrieveButton");
+const fontSize = document.getElementById("fontSize");
+const ctx = canvas.getContext("2d");
+
+colorPicker.addEventListener("change", (e) => {
+  ctx.strokeStyle = e.target.value;
+});
+
+canvas.addEventListener("mousedown", (event) => {
+  isDrawing = true;
+  lastX = event.offSetX;
+  lastY = event.offSetY;
+});
+
+canvas.addEventListener("mousemove", (event) => {
+  if (isDrawing) {
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+
+    lastX = event.offsetX;
+    lastY = event.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
+
+canvasColor.addEventListener("change", (e) => {
+  ctx.fillStyle = e.target.value;
+  ctx.fillRect(0, 0, 800, 500);
+});
+
+fontSize.addEventListener("change", (e) => {
+  ctx.lineWidth = e.target.value;
+});
+
+clearButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+saveButton.addEventListener("click", () => {
+  localStorage.setItem("canvasContents", canvas.toDataURL());
+
+  let link = document.createElement("a");
+
+  link.download = "my-canvas.png";
+
+  link.href = canvas.toDataURL();
+
+  link.click();
+});
+
+retrieveButton.addEventListener("click", () => {
+  let savedCanvas = localStorage.getItem("canvasContents");
+
+  if (savedCanvas) {
+    let img = new Image();
+    img.src = savedCanvas;
+    ctx.drawImage(img, 0, 0);
+  }
+});
